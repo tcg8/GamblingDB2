c68bc032ff524d6fefec87f80f864959e9644f94
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-02-21 12:51:09 -0500

    Update db.php

--- dbtest/db.php
+++ dbtest/db.php
@@ -1,29 +1,85 @@
 <?php
-//for this we'll turn on error output so we can try to see any problems on the screen
-//this will be active for any script that includes/requires this one
-ini_set('display_errors', 1);
+#turn error reporting on
+ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-function getDB(){
-    global $db;
-    //this function returns an existing connection or creates a new one if needed
-    //and assigns it to the $db variable
-    if(!isset($db)) {
-        try{
-            //__DIR__ helps get the correct path regardless of where the file is being called from
-            //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
-            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
-            //use the variables from config to populate our connection
-            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-            //using the PDO connector create a new connect to the DB
-            //if no error occurs we're connected
-            $db = new PDO($connection_string, $dbuser, $dbpass);
-        }
-    catch(Exception $e){
-            var_export($e);
-            $db = null;
-        }
+//pull in db.php so we can access the variables from it
+require_once (__DIR__ . "/../lib/db.php");
+$count = 0;
+try{
+    //name each sql file in a way that it'll sort correctly to run in the correct order
+    //my samples prefix filenames with #_
+    /***
+     * Finds all .sql files in structure directory.
+     * Generates an array keyed by filename and valued as raw SQL from file
+     * The .sql is important here since this directory may have other files types
+     */
+    foreach(glob(__DIR__ . "/*.sql") as $filename){
+        $sql[$filename] = file_get_contents($filename);
     }
-    return $db;
+    if(isset($sql) && $sql && count($sql) > 0){
+        /***
+         * Sort the array so queries are executed in anticipated order.
+         * Be careful with naming, 1-9 is fine, 1-10 has 10 run after #1 due to string sorting.
+         */
+        ksort($sql);
+        echo "<br><pre>" . var_export($sql, true) . "</pre><br>";
+        //connect to DB
+        $db = getDB();
+        /***
+         * Let's make this function a bit smarter to save DB calls for small dev plans (i.e., heroku)
+         */
+        $stmt = $db->prepare("show tables");
+        $stmt->execute();
+        $count++;
+        $tables = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $t = [];
+        //convert it to a flat array
+        foreach($tables as $row){
+            foreach($row as $key => $value) {
+                array_push($t, $value);
+            }
+        }
+        foreach($sql as $key => $value){
+            echo "<br>Running: " . $key;
+            $lines = explode("(", $value, 2);
+            if(count($lines) > 0){
+                //these lines attempt to extract the table info from any create commands
+                //to determine if they command should be skipped or not
+                $line = $lines[0];
+                //clear out duplicate whitespace
+                $line = preg_replace('!\s+!', ' ', $line);
+                //remove create table command
+                $line = str_ireplace("create table", "", $line);
+                //remove if not exists command
+                $line = str_ireplace("if not exists", "", $line);
+                //remove backticks
+                $line = str_ireplace("`","",$line);
+                //trim whitespace in front and back
+                $line = trim($line);
+                if (in_array($line, $t)){
+                    echo "<br>Blocked from running, table found in 'show tables' results.<br>";
+                    continue;
+                }
+            }
+            $stmt = $db->prepare($value);
+            $result = $stmt->execute();
+            $count++;
+            $error = $stmt->errorInfo();
+            if($error && $error[0] !== '00000'){
+                echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+            }
+            echo "<br>$key result: " . ($result>0?"Success":"Fail") . "<br>";
+        }
+        echo "<br> Init complete, used approximately $count db calls.<br>";
+    }
+    else{
+        echo "Didn't find any files, please check the directory/directory contents/permissions";
+    }
+    $db = null;
+}
+catch(Exception $e){
+    echo $e->getMessage();
+    exit("Something went wrong");
 }
 ?>
