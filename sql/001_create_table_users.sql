2c1b5ea017d8549d225b7db3b19b042ca968c596
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-02-28 12:07:29 -0500

    Update 001_create_table_users.sql

--- sql/001_create_table_users.sql
+++ sql/001_create_table_users.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS `Users` (
 	`id` INT NOT NULL AUTO_INCREMENT
 	,`email` VARCHAR(100) NOT NULL
-	, `username` VARCHAR(100) NOT NULL
+	,`username` VARCHAR(60) default ''
 	,`password` VARCHAR(60) NOT NULL
 	,`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 	,PRIMARY KEY (`id`)
 	,UNIQUE (`email`)
-	,UNIQUE (`username`)
 	)
