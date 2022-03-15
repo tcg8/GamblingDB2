6b4da8fbd980c443cd6180e41802030d0284d1b8
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-15 15:06:43 -0400

    Delete 014_create_teams_table.sql

--- sql/014_create_teams_table.sql
+++
@@ -1,9 +0,0 @@
-create table Teams(
-    `id`                 int auto_increment,
-    `league_name`        varchar(30) not null,
-    `home_name`          varchar(30) not null,
-    `away_name`          varchar(30) not null,
-    `created timestamp`  default CURRENT_TIMESTAMP,
-    `event_id`           int not null,
-    primary key (id)
-)
