78254b93b8329bc1ddaff65e45fea2106ca36111
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-14 20:15:44 -0400

    Update 015_create_team_statistics_table.sql

--- sql/015_create_team_statistics_table.sql
+++ sql/015_create_team_statistics_table.sql
@@ -12,7 +12,4 @@ CREATE TABLE IF NOT EXISTS  `TeamStatistics`
     `created`           timestamp default current_timestamp,
     `modified`          timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`id`),
-   // FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
-    //FOREIGN KEY (`competition_id`) REFERENCES Competitions(`id`),
-    //UNIQUE KEY `user_comp` (`user_id`, `competition_id`)
 )
