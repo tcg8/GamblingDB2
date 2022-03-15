e88f81ec94502d3610f96c2f828eb2a2f2202247
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-15 14:58:22 -0400

    Create create_user_team_table.sql

---
+++ sql/create_user_team_table.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS  `user_team`
+(
+    `id`                int auto_increment not null,
+    `user_owner`        int,
+    
+    `eplayer1_id`           int,
+    `eplayer2_id`           int,
+    `eplayer3_id`           int,
+    `eplayer4_id`           int,
+    `eplayer5_id`           int,
+    `eplayer6_id`           int,
+    `eplayer7_id`           int,
+    `eplayer8_id`           int,
+    `eplayer9_id`           int,
+    `eplayer10_id`           int,
+    
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_owner`) REFERENCES Users(`id`),
+    
+    FOREIGN KEY (`eplayer1_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer2_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer3_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer4_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer5_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer6_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer7_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer8_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer9_id`) REFERENCES eplayer(`id`),
+    FOREIGN KEY (`eplayer10_id`) REFERENCES eplayer(`id`),
+)
