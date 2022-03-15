79c93d49fb924ec273d8fb6e936fe26bdb452b95
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-15 15:03:58 -0400

    Create create_eplayer_table.sql

---
+++ sql/create_eplayer_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS  `eplayer`
+(
+    `id`                int auto_increment not null,
+    `name`              varchar(30) not null,
+    `position`          varchar(30) not null,
+    `KDA`               double,
+    `GPM`               double,
+    `CSA`               double,
+    `eteam`             varchar(30) not null,
+    
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`name`),
+)
