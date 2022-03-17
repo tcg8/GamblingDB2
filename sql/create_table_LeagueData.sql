fec15eeb363700605388b2b904680b7f3b5d1c07
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-17 14:25:16 -0400

    Update create_table_LeagueData.sql

--- sql/create_table_LeagueData.sql
+++ sql/create_table_LeagueData.sql
@@ -1,10 +1,10 @@
 USE League_Gamble;
 CREATE TABLE IF NOT EXISTS `LeagueData` (
     `id` int auto_increment not null,
-    `leaguename` varchar(100),
-    `hometeam` varchar(100),
-    `awayteam` varchar(100), 
+    `leaguename` varchar(255),
+    `hometeam` varchar(255),
+    `awayteam` varchar(255), 
     `eventdate` datetime,
-    `eventID` int(100),
+    `eventID` int(255),
     PRIMARY KEY (`id`)
 );
