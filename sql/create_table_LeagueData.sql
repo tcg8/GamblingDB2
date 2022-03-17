62a3f063de56e3beaa7d7eca1e081fa9f7a210b8
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-17 13:52:37 -0400

    Create create_table_LeagueData.sql

---
+++ sql/create_table_LeagueData.sql
@@ -0,0 +1,9 @@
+CREATE TABLE LeagueData (
+    `id` int auto_increment not null,
+    `leaguename` varchar(100),
+    `hometeam` varchar(100),
+    `awayteam` varchar(100), 
+    `eventdate` datetime,
+    `eventID` int(100),
+    PRIMARY KEY (`id`)
+);
