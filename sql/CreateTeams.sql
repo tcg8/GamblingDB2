ee26047ea24034e8fca0d57ec0fbdf5f73128e5c
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-25 16:23:32 -0400

    Update CreateTeams.sql

--- sql/CreateTeams.sql
+++ sql/CreateTeams.sql
@@ -1,6 +1,5 @@
 USE League_Gamble;
 CREATE TABLE CreateTeams (
   username  varchar(255),
-  key varchar(255),
-  FOREIGN KEY ('key') REFERENCES CreateGroups ('code')
+  key varchar(255) FOREIGN KEY REFERENCES CreateGroups(code)
 );
