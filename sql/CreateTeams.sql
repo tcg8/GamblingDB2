777fc53ffd2bd1fdc34d90a106a0ceca446fc1a5
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-25 16:39:20 -0400

    Update CreateTeams.sql

--- sql/CreateTeams.sql
+++ sql/CreateTeams.sql
@@ -1,4 +1,5 @@
 CREATE TABLE CreateTeams (
     players varchar(255) PRIMARY KEY,     
-    code varchar(255) FOREIGN KEY REFERENCES CreateGroups(code)
+    code varchar(255),
+    FOREIGN KEY code REFERENCES CreateGroups(code)
 );
