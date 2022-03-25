b89f83040c92e063dfe048dbdaca02dc9c522261
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-25 18:51:59 -0400

    Update create_table_HistoricalData.sql

--- sql/create_table_HistoricalData.sql
+++ sql/create_table_HistoricalData.sql
@@ -1,14 +1,12 @@
-USE League_Gamble;
-CREATE TABLE IF NOT EXISTS `HistoricalData` (
-    `id` int auto_increment not null,
-    `Name` varchar(255),
-    `Season` varchar(255),
-    `url` varchar(255),
-    `Region` varchar(255),
-    `Games` int(255),
-    `trimWin_rate` decimal(5,2),
-    `KD` decimal(3,2),
-    `GPM` int(255),
-    `GDM` int(255),
-    PRIMARY KEY (`id`)
+CREATE TABLE HistoricalData (
+    id int auto_increment not null,
+    Name varchar(255) PRIMARY KEY,
+    Season varchar(255),
+    url varchar(255),
+    Region varchar(255),
+    Games int(255),
+    trimWin_rate decimal(5,2),
+    KD decimal(3,2),
+    GPM int(255),
+    GDM int(255)
 );
