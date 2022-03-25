da18245d88e17384e45e49aeec7ee66a5b9efb58
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-24 20:09:33 -0400

    Update create_table_groups.sql

--- sql/create_table_groups.sql
+++ sql/create_table_groups.sql
@@ -2,5 +2,5 @@ USE League_Gamble;
 CREATE TABLE `Groups` (
   `Name`              varchar(255) PRIMARY KEY NOT NULL,
   `Code`              varchar(255) NOT NULL, 
-  `Players`           int default 0,
+  `Players`           int default 0
 );
