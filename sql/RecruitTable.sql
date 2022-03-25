2e3d70a2fd1f7db7fd970518f0a6f2c1c3b19832
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-03-25 18:45:34 -0400

    Update RecruitTable.sql

--- sql/RecruitTable.sql
+++ sql/RecruitTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE RecruitTable (
     players varchar(255) PRIMARY KEY,  
     Eteam varchar(255),
-    FOREIGN KEY (Eteam) REFERENCES HistoricalData(Name)
+    FOREIGN KEY (Eteam) REFERENCES HistoricalData(`Name`)
 );
