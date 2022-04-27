df4648f950d10dbc9755e92f1c15ddbe5f7312ad
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-04-27 12:13:38 -0400

    Create smsHope.php

---
+++ smsHope.php
@@ -0,0 +1,17 @@
+<?php
+if ( isset( $_REQUEST ) && !empty( $_REQUEST ) ) {
+ if (
+ isset( $_REQUEST['phoneNumber'], $_REQUEST['carrier'], $_REQUEST['smsMessage'] ) &&
+  !empty( $_REQUEST['phoneNumber'] ) &&
+  !empty( $_REQUEST['carrier'] )
+ ) {
+  $message = wordwrap( $_REQUEST['smsMessage'], 70 );
+  $to = $_REQUEST['phoneNumber'] . '@' . $_REQUEST['carrier'];
+  $result = @mail( $to, '', $message );
+  print 'Message was sent to ' . $to;
+ } else {
+  print 'Not all information was submitted.';
+ }
+}
+
+# citation: 4/27/22 12:13pm https://code.tutsplus.com/tutorials/how-to-send-text-messages-with-php--net-17693 
