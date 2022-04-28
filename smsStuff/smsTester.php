324e5b5551f0c52553fd70295a65c2c4e008b8d0
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-04-28 14:29:30 -0400

    Update smsTester.php

--- smsStuff/smsTester.php
+++ smsStuff/smsTester.php
@@ -1,5 +1,5 @@
 <?php
  
-var_dump( mail( '##########@vtext.com', '', 'This was sent with PHP.' ) ); // bool(true)
+var_dump( mail( 'tcg8@njit.edu', '', 'This was sent with PHP.' ) ); // bool(true)
  #citation https://code.tutsplus.com/tutorials/how-to-send-text-messages-with-php--net-17693 4/28/2022 2pm
 ?>
