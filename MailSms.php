e89cc90cb78431b76d087a0c3d2c944b3cb59690
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-05-02 12:04:10 -0400

    Create MailSms.php

---
+++ MailSms.php
@@ -0,0 +1,25 @@
+require 'class.phpmailer.php';
+ 
+// Instantiate Class
+$mail = new PHPMailer();
+ 
+// Set up SMTP
+$mail->IsSMTP();                // Sets up a SMTP connection
+$mail->SMTPDebug  = 2;          // This will print debugging info
+$mail->SMTPAuth = true;         // Connection with the SMTP does require authorization
+$mail->SMTPSecure = "tls";      // Connect using a TLS connection
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587;
+$mail->Encoding = '7bit';       // SMS uses 7-bit encoding
+ 
+// Authentication
+$mail->Username   = "tcg8@njit.edu"; // Login
+$mail->Password   = "Just1morebeer"; // Password
+ 
+// Compose
+$mail->Subject = "Testing";     // Subject (which isn't required)
+$mail->Body = "Testing";        // Body of our message
+ 
+// Send To
+$mail->AddAddress( tristangibs@gmail.com" ); // Where to send it
+var_dump( $mail->send() );      // Send!
