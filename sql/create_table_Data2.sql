a9220ddc6dbc0990f4f66283ec4cb7e3a2ce8fae
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-05-05 19:32:28 -0400

    Create create_table_Data2.sql

---
+++ sql/create_table_Data2.sql
@@ -0,0 +1,10 @@
+USE League_Gamble;
+CREATE TABLE IF NOT EXISTS `Data2` (
+    `id` int auto_increment not null,
+    `reference_id` int(50),
+    `Win_Team` varchar(255),
+    `Home_Odds` varchar(255), 
+    `Away_Odds` varchar(255),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`reference_id`) REFERENCES LeagueData(`id`),
+);
