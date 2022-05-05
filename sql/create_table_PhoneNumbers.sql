f70e59d6b804052b02b0fc24064246f65d2a52eb
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-05-05 19:40:04 -0400

    Update create_table_PhoneNumbers.sql

--- sql/create_table_PhoneNumbers.sql
+++ sql/create_table_PhoneNumbers.sql
@@ -2,5 +2,5 @@ USE League_Gamble;
 CREATE TABLE IF NOT EXISTS `PhoneNumbers` (
     `username` varchar(255) PRIMARY KEY NOT NULL,
     `phone` int(16),
-    FOREIGN KEY (`username`) REFERENCES Users(`username`);
+    FOREIGN KEY (`username`) REFERENCES Users(`username`)
 );
