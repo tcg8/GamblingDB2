1a20ea1a7313dfd7271d001107ce7c09f55523a1
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-05-10 17:34:40 -0400

    Update create_table_betting_history.sql

--- sql/create_table_betting_history.sql
+++ sql/create_table_betting_history.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `Betting_History` (
     `id` int auto_increment not null,
-    `Username` varchar(255),  
-    `Hometeam` varchar(255),
-    `Awayteam` varchar(255),
-    `teambet` varchar(255),
+    `Username` varchar(255) default '',  
+    `Hometeam` varchar(255) default '',
+    `Awayteam` varchar(255) default '',
+    `teambet` varchar(255) default '',
     `Won_or_Loss` varchar(255) default '',
     `Betting_Score` int(255) default 0,
     PRIMARY KEY (`id`),
