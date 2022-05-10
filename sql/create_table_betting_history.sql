37aed654ca1a3a0c0daef932994bc39f08b503e1
tcg8 <50150270+tcg8@users.noreply.github.com>
2022-05-10 16:12:50 -0400

    Create create_table_betting_history.sql

---
+++ sql/create_table_betting_history.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Betting_History (
+    id int auto_increment not null
+    Username varchar(255),  
+    Hometeam varchar(255),
+    Awayteam varchar(255),
+    teambet varchar(255),
+    Won_or_Loss varchar(255) default '',
+    Betting_Score int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (Username) REFERENCES Users(username)
+);
